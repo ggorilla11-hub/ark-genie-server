44255abd0e3be088ebb322f3a1da258a6ded3688
ggorilla11-hub <ggorilla11@gmail.com>
2025-12-13 06:21:35 +0900

    Update server.js

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const WebSocket = require('ws');
 const twilio = require('twilio');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
